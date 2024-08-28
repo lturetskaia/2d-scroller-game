8c1ebfb7fce97ac3615027123f8d975d16397570
Larisa Turetskaia <lturetskaja@gmail.com>
2024-08-29 02:44:19 +0300

    Delete unused code

--- sketch.js
+++ sketch.js
@@ -183,32 +183,6 @@ function draw() {
   //loosing lives when contacting enemy/canyon
   charLooseLife();
 
-  // check contact with enemy
-  // if (gameChar.isAlive && gameChar.checkEnemyCollision(enemy) && gameChar.looseLife()) {
-  //   soundButton.isToggle ? sound.playSound("fall") : null;
-
-  //   if (gameChar.lives > 0) {
-  //     gameChar.isEnemyContact = true;
-  //     game.resetToPrevScore();
-  //   } else {
-  //     game.resetScore();
-  //     game.isGameOver = true;
-  //   }
-  // }
-
-  //   //check if the character looses lives or dies
-  //   if (gameChar.isAlive && gameChar.looseLife()) {
-  //     soundButton.isToggle ? sound.playSound("fall") : null;
-
-  //     if (gameChar.lives > 0) {
-  //       game.resetToPrevScore();
-  //       startGame();
-  //     } else {
-  //       game.resetScore();
-  //       game.isGameOver = true;
-  //     }
-  //   }
-
   // check if the flagpole is reached
   if (!flagpole.isReached) {
     flagpole.checkFlagpole(gameChar.xPos);
