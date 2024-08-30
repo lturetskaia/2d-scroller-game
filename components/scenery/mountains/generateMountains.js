4d77823edeacde31b412b36c8803c027b12326b4
Larisa Turetskaia <lturetskaja@gmail.com>
2024-08-30 19:55:29 +0300

    Fix canyon generation

--- components/scenery/mountains/generateMountains.js
+++ components/scenery/mountains/generateMountains.js
@@ -8,18 +8,31 @@ function generateMountains() {
     colours = { top: color(96, 64, 31), bottom: color(115, 77, 38) };
   }
 
-  for (var i = 0; i < 8; i++) {
+    labelGenerateMountains: while (generatedMountains.length < 8) {
     //generate mountains
     var mountainSize = Number(random(0.5, 0.9).toFixed(2));
-    var mountainWidth = 260;
+    var mountainWidth = 340;
     var mountRightEdge = mountainWidth * mountainSize;
     var startXPos = 100;
     var endXPos = scrollingSpace - mountRightEdge;
     var mountainXpos = round(random(startXPos, endXPos));
+
+    //check for collision with canyons
+    var mountainCentre = mountainXpos + (mountainWidth * mountainSize) / 2;
+    for (var j = 0; j < canyons.length; j++) {
+      var canyonCentre = canyons[j].xPos + canyons[j].width / 2;
+      var dist = abs(mountainCentre - canyonCentre);
+      var isOverlap = dist < canyons[j].width / 2 + (mountainWidth * mountainSize) / 2;
+      if (isOverlap) {
+        console.log(`Overlap`);
+        continue labelGenerateMountains;
+      }
+    }
 
     var mountain = new Mountain(mountainXpos, floorPosY, mountainSize, colours);
     generatedMountains.push(mountain);
   }
 
   mountains = [...generatedMountains];
+  console.log(mountains);
 }
--- components/scenery/mountains/mountain.js
+++ components/scenery/mountains/mountain.js
@@ -8,34 +8,35 @@ class Mountain {
 
   drawMountain() {
     fill(this.mountColor.bottom);
-    
-    //mountains
+
     triangle(
-      this.xPos,
+      //big
+      this.xPos + 80 * this.size,
       this.yPos,
-      this.xPos + 130 * this.size,
+      this.xPos + 210 * this.size,
       this.yPos - 332 * this.size,
-      this.xPos + 260 * this.size,
+      this.xPos + 340 * this.size,
       this.yPos
     );
     triangle(
-      this.xPos - 80 * this.size,
+      //small
+      this.xPos,
       this.yPos,
-      this.xPos + 10 * this.size,
+      this.xPos + 90 * this.size,
       this.yPos - 212 * this.size,
-      this.xPos + 80 * this.size,
+      this.xPos + 170 * this.size,
       this.yPos
     );
 
     // snow on top
     fill(this.mountColor.top);
     beginShape();
-    vertex(this.xPos + 130 * this.size, this.yPos - 332 * this.size);
-    vertex(this.xPos + 90 * this.size, this.yPos - 232 * this.size);
-    vertex(this.xPos + 110 * this.size, this.yPos - 252 * this.size);
-    vertex(this.xPos + 130 * this.size, this.yPos - 212 * this.size);
-    vertex(this.xPos + 160 * this.size, this.yPos - 242 * this.size);
+    vertex(this.xPos + 210 * this.size, this.yPos - 332 * this.size);
     vertex(this.xPos + 170 * this.size, this.yPos - 232 * this.size);
+    vertex(this.xPos + 190 * this.size, this.yPos - 252 * this.size);
+    vertex(this.xPos + 210 * this.size, this.yPos - 212 * this.size);
+    vertex(this.xPos + 240 * this.size, this.yPos - 242 * this.size);
+    vertex(this.xPos + 250 * this.size, this.yPos - 232 * this.size);
     endShape();
   }
 }
